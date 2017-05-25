d9dfe9a0cd76eb74293e19726f334b733bd76458
juniorh <ri24l_pwt@yahoo.com>
2017-05-24 23:30:49 -0400

    print exponen, primes, PC, modulus

--- encryptDecript/go/generatePrivatePublicKey.go
+++ encryptDecript/go/generatePrivatePublicKey.go
@@ -11,7 +11,8 @@ import(
 
 func main(){
   Priv,_ := rsa.GenerateKey(rand.Reader, 128)
-
+  Pub := Priv.PublicKey
+  PCValues := Priv.Precomputed
   PrivASN1 := x509.MarshalPKCS1PrivateKey(Priv)
   PubASN1, err := x509.MarshalPKIXPublicKey(&Priv.PublicKey)
 
@@ -29,7 +30,18 @@ func main(){
     Bytes: PubASN1,
   })
 
-  ioutil.WriteFile("/ssl/key.pem", PrivBytes, 0644)
-  ioutil.WriteFile("/ssl/key.pub", PubBytes, 0644)
+  fmt.Println("Private Key:", Priv)
+  fmt.Println("Private Exponent :", Priv.D.String())
+  fmt.Printf("Primes : %s %s \n", Priv.Primes[0].String(), Priv.Primes[1].String())
+  fmt.Printf("Precomputed Values : Dp[%s] Dq[%s] Qinv[%s]\n", PCValues.Dp.String(), PCValues.Dq.String(), PCValues.Qinv.String())
+
+  fmt.Println()
+  fmt.Println("Public key ", pubKey)
+  fmt.Println("Public Exponent : ", pubKey.E)
+  fmt.Println("Modulus : ", pubKey.N.String())
+
+  fmt.Println("Save file to ssl/key.pem & ssl/key.pub")
+  ioutil.WriteFile("ssl/key.pem", PrivBytes, 0644)
+  ioutil.WriteFile("ssl/key.pub", PubBytes, 0644)
 }
 
